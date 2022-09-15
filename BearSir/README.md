f012bf4c99ea9c4592090a75f82e91d607c4b6a4
iownE9 <iownE9@163.com>
2022-09-15 13:48:51 +0800

    hw04-C

--- BearSir/README.md
+++ BearSir/README.md
@@ -3,3 +3,5 @@
 proj02 PROBLEM 7
 
 hw03 Q11: Multiple References
+
+hw04 Generator的定义
--- BearSir/lectures/lec14-demo.py
+++ BearSir/lectures/lec14-demo.py
@@ -1,57 +1,32 @@
-def make_withdraw1(balance)
-     Return a Withdarw function that withdraws money
-    from an account with initial balance of BALANCE.
+class Countdown:
+    """ Count down to zero.
+    >>> list(Countdown(5))
+    [5, 4, 3, 2, 1]
+    >>> for x in Countdown(3):
+    ...     print(x)
+    3
+    2
+    1
+    """
+
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        v = self.start
+        while v > 0:
+            yield v
+            v -= 1
+
+
+def countdown(n):
+    """ Lazily return n ~ 1.
     
-     withdraw = make_withdraw1(100)
-     withdraw(25)
-    75
-     withdraw(25)
-    50
-     withdraw(60)
-    'Insufficient funds!'
-     withdraw(15)
-    35
-    
-    def withdraw(amount)
-        nonlocal balance
-        if amount  balance
-            return 'Insufficient funds!'
-        balance -= amount
-        return balance
-    return withdraw 
-
-
-def make_withdraw2(balance)
-     Return a Withdarw function that withdraws money
-    from an account with initial balance of BALANCE.
-    
-     withdraw = make_withdraw2(100)
-     withdraw(25)
-    75
-     withdraw(25)
-    50
-     withdraw(60)
-    'Insufficient funds!'
-     withdraw(15)
-    35
-    
-    b = [balance]
-    def withdraw(amount)
-        if amount  b[0]
-            return 'Insufficient funds!'
-        b[0] -= amount
-        return b[0]
-    return withdraw 
-
-def f(x)
-    x = 4
-    def g(y)
-        def h(z)
-            nonlocal x
-            x = x + 1
-            return x + y + z
-        return h
-    return g
-
-
-
+    >>> list(countdown(4))
+    [4, 3, 2, 1]
+    >>> list(countdown(8))
+    [8, 7, 6, 5, 4, 3, 2, 1]
+    """
+    if n > 0:
+        yield n
+        yield from countdown(n - 1)
